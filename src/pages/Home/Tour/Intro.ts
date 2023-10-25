7f1108917b0d3cb832d22d6bd94d70d8ed8a3e51
Irodans <jobjel@mail.dk>
2023-10-25 16:29:04 +0200

    ..

--- src/pages/Home/Tour/Intro.ts
+++ src/pages/Home/Tour/Intro.ts
@@ -9,103 +9,69 @@ interface IntroTour
 
 export class Intro 
 {
-
   intro: any = null;
-  page: HTMLElement = null;
-  leftmenu: HTMLElement = null;
-  readmeDIv: HTMLElement = null;
-  connection: HTMLElement = null;
+  page:HTMLElement = null;
   iframe: HTMLIFrameElement = null;
   introbutton: HTMLButtonElement = null;
   introDivtour: NodeListOf<HTMLDivElement> = null;
 
-
-  constructor() 
-  {
-    
+  constructor() {
     this.iframe = document.querySelector("#myIframe") as HTMLIFrameElement;
 
-    if(this.iframe)
-      this.iframe.style.display = "none";
+    if (this.iframe) this.iframe.style.display = "none";
 
     this.introbutton = document.querySelector("#intro") as HTMLButtonElement;
-    
-    if(this.introbutton) 
-      this.introbutton.addEventListener("click", () => this.starttour());
-    
-    if(this.iframe) 
-      this.page = this.iframe.contentDocument.body.querySelector(".image_container") as HTMLDivElement;
 
-    if(this.page) 
-    {
-      this.leftmenu = this.page.querySelector("#leftmenu") as HTMLDivElement;
+    if (this.iframe) {
+      this.page = this.iframe.contentDocument.body;
       this.introDivtour = this.page.querySelectorAll(".introtour") as NodeListOf<HTMLDivElement>;
-    }    
-  }
+    }
 
-  private getIntrotourObjects(introDivtour: NodeListOf<HTMLDivElement>): IntroTour[]
-  {
-    let introtourObjects: IntroTour[] = [];
-
-    introDivtour.forEach((introtourElement) =>
-    {
-
-      let titleElement = introtourElement.querySelector('#title') as HTMLLabelElement;
-      let introElement = introtourElement.querySelector('#intro')as HTMLLabelElement;
-      let elementElement = introtourElement.querySelector('#element') as HTMLLabelElement
- 
-      
-    
-      if (titleElement && introElement)
+    fetch('documentation/tour.json')
+      .then(response => response.json())
+      .then(data => 
       {
-        let element = null ;
-
-        if(elementElement)
-        {
-          element = this.page.querySelector(elementElement.textContent) as HTMLElement;
-
-          if(elementElement.textContent == ".image_container")
-            element = this.page
+        if (this.introbutton && this.introDivtour) {
+          let introtourObjects = data.intro as IntroTour[];
+  
+          this.introbutton.addEventListener("click", () => this.starttour(introtourObjects));
+        } else {
+          console.error('Missing elements or JSON data.');
         }
-
-        let introtour : IntroTour = 
-        {
-          title: titleElement.textContent,
-          intro: introElement.textContent,
-          element: element
-        }
-
-        introtourObjects.push(introtour);
-      };
-    })
-    return introtourObjects
+      })
+      .catch(error => {
+        console.error('Error fetching JSON data:', error);
+      });
   }
 
-  public starttour() : void
+  public starttour(introtourObjects: IntroTour[]): void 
   {
     this.intro = introJs();
-      this.tour(this.introDivtour);
+    console.log("1");
+    this.tour(introtourObjects);
 
-    this.iframe.style.display = "block";
+    if (this.iframe) this.iframe.style.display = "block";
+
     this.intro.onexit(() => {
-      this.iframe.style.display = "none";
+      if (this.iframe) this.iframe.style.display = "none";
     });
+
     this.intro.start();
   }
 
-
-  private tour(introDivtour : NodeListOf<HTMLDivElement>): void 
+  private tour(introtourObjects: IntroTour[]): void 
   {
-    let introtourObjects = this.getIntrotourObjects(introDivtour);
-    let steps = introtourObjects.map((introtour) => ({
-      element: introtour.element, 
-      title: introtour.title,
-      intro: introtour.intro,
-    }));
-
-    this.intro.setOptions(
-    {
-      steps: steps,
-    });
+      let steps = introtourObjects.map((introtour) => 
+      ({
+        element: introtour.element instanceof HTMLElement
+        ? introtour.element
+        : this.page.querySelector(introtour.element) as HTMLElement,
+        title: introtour.title,
+        intro: introtour.intro,
+      }));
+      this.intro.setOptions
+      ({
+        steps: steps,
+      });
   }
 }
